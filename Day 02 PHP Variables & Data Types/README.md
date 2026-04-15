f947c3e91c55c2b8eef93fdf04ef83afc559ab0b
muhammad-husnain-ali-web <mhusnainali913@gmail.com>
2026-04-15 12:19:23 +0500

    Day 2 - PHP Variables & Data Types

---
+++ Day 02 PHP Variables & Data Types/README.md
@@ -0,0 +1,18 @@
+# Day 2 - PHP Variables & Data Types
+
+## What I Learned
+- Variables in PHP
+- Data types (string, int, float, boolean)
+- Using echo with variables
+- var_dump()
+
+## Code Example
+```php
+<?php
+$name = "Ali";
+$age = 20;
+
+echo "My name is $name";
+echo "<br>";
+echo "My age is $age";
+?>
---
+++ Day 02 PHP Variables & Data Types/index.php
@@ -0,0 +1,40 @@
+<?php
+
+// String (text)
+$name = "Ali";
+// Integer (number)
+$age = 20;
+// Float (decimal)
+$price = 99.99;
+// Boolean (true/false)
+$isStudent = true;
+
+echo "<h1>Variables Practice</h1> <br>";
+
+$name = "Ali";
+$age = 20;
+
+echo var_dump($isStudent);
+echo "<br>";
+
+echo "My name is " . $name;
+echo "<br>";
+echo "My age is " . $age;
+echo "<br>";
+
+$num1 = 10;
+$num2 = 5;
+
+echo "Addition: " . ($num1 + $num2);
+echo "<br>";
+echo "Subtraction: " . ($num1 - $num2);
+echo "<br>";
+echo "Multiplication: " . ($num1 * $num2);
+echo "<br>";
+echo "Division: " . ($num1 / $num2);
+echo "<br>";
+echo "Module: " . ($num1 % $num2);
+
+
+
+?>
